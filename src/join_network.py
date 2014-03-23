c070b2b9ae3cdb932a7530db2d6d13847b2c412c
Scott Cunningham <cunninsc@tcd.ie>
2014-03-23 20:57:05 +0000

    keys

--- src/join_network.py
+++ src/join_network.py
@@ -2,6 +2,19 @@
 
 from pydht import DHT
 import sys
+import os
+
+def get_keys(dir):
+    keys = {}
+    for filename in os.listdir(dir):
+        # form privkey-node215132971667130584370402257404962689265.pem
+        uid = filename[12:]
+        uid = uid.split(".pem")[0]
+        path = os.path.join(dir, filename)
+        f = open(path, 'r')
+        keys[uid] = f.read()
+        f.close()
+    return keys
 
 def join_network(hostname, port, boot_host, boot_port):
     n = DHT(hostname, port)
@@ -10,13 +23,15 @@ def join_network(hostname, port, boot_host, boot_port):
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print "Usage:", sys.argv[0], "HOSTNAME PORT BOOTSTRAP_HOSTNAME BOOTSTAP_PORT"
-        sys.exit(-1)
-
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    boot_host = sys.argv[3]
-    boot_port = int(sys.argv[4])
+        host = "localhost"
+        port = 4000
+        boot_host = host
+        boot_port = 3000
+    else: 
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+        boot_host = sys.argv[3]
+        boot_port = int(sys.argv[4])
 
     print "Joining network"
     node = join_network(host, port, boot_host, boot_port)
@@ -24,9 +39,15 @@ if __name__ == '__main__':
 
     while True:
         k = raw_input("query >>> ")
-        try:
-            v = node[k]
-            print "results for: {}".format(k)
-            print v
-        except KeyError:
-            print "no results for: {}".format(k)
+        kk = k.split(" ")
+        if len(kk) > 1:
+            key, val = kk
+            print "storing key {} in dht with val {}".format(key, val)
+            node[key] = val
+        else:
+            try:
+                v = node[k]
+                print "results for: {}".format(k)
+                print v
+            except KeyError:
+                print "no results for: {}".format(k)
