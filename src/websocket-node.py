6f5ab51236217154892807c564ada33a7c3b6135
Scott Cunningham <cunninsc@tcd.ie>
2014-03-24 15:58:45 +0000

    fixed websocket node

--- src/websocket-node.py
+++ src/websocket-node.py
@@ -6,20 +6,20 @@ from autobahn.twisted.websocket import WebSocketServerProtocol, \
 from pydht import DHT
 import sys
 import json
-import uuid
 import tempfile
+from join_network import get_keys
 
 from twisted.python import log
 from twisted.internet import reactor
-
-import json
-import hashlib
 
 NUM_NODES = 2
 PORT_START = 3001
 HOST = "localhost"
 
-node = DHT("localhost", PORT_START + 50, boot_host=HOST, boot_port=PORT_START- 1)
+print "Creating node, joining network"
+keys = get_keys("keys")
+node = DHT("localhost", PORT_START + 50, boot_host=HOST, boot_port=PORT_START- 1, keys=keys)
+print "Done."
 
 class MyServerProtocol(WebSocketServerProtocol):
 
@@ -34,19 +34,26 @@ class MyServerProtocol(WebSocketServerProtocol):
  
    def onMessage(self, payload, isBinary):
       global node
-      raw_message = payload.decode('utf-8')
+      raw_message = str(payload) #.decode('utf8', errors='ignore')
       message = json.loads(raw_message)
 
       print "Got message: {}".format(raw_message)
       
-      key = message["body"]
+      key = message["key"]
       
+      if message["type"] == "downvote":
+          print "downvote", key
+          node.downvote(key)
+
       if message["type"] == "publish":
           print "publish req"
-          content = message["body"]
-          print "storing now"
-          key = node.publish(content, content) 
+          content = str(message["body"]) #.encode('utf8', errors='ignore')
+          key = str(key) #).encode('utf8', errors='ignore')
           print "hash is", key
+          print "content is", content
+          print "storing now"
+          content = json.dumps(content)
+          node.publish(key, content)
           self.sendMessage(json.dumps({"type" : "publish", "error" : False, "value" : key}), False)
           print "stored"
 
@@ -54,31 +61,28 @@ class MyServerProtocol(WebSocketServerProtocol):
           print "lookup"
           try:
             print "Looking up key {}".format(key)
-            value = node[key]
-            if isBinary:
-              print("Binary message received: {} bytes".format(len(payload)))
-            else:
-              print("Text message received: {}".format(str(payload.decode('utf8'))))
-
-            ## echo back message verbatim
+            value = node.retrieve(key)
+            value = json.loads(value)
+            print "decrypted is", value
             self.sendMessage(json.dumps({"type" : "lookup", "error" : False, "value" : value}), isBinary)
      
           except KeyError:
             print "Key not found in DHT"
             # do something error-y, return something bad
             self.sendMessage(json.dumps({"type" : "lookup", "error" : True, "value" : "key not found!"}), False)
-         
-
-      def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {}".format(reason))
+    
+   def onClose(self, wasClean, code, reason):
+     print("WebSocket connection closed: {}".format(reason))
 
 
 if __name__ == '__main__':
    log.startLogging(sys.stdout)
 
 
+   print "Creating websocket frontend handler"
    factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
    factory.protocol = MyServerProtocol
+   print "Done, starting up"
 
    reactor.listenTCP(9000, factory)
    reactor.run()
